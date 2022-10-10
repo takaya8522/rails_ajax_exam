6c706304f98a9770a5ebf52809724d9210a6e25b
takaya suzuki <suzuki8522@yahoo.co.jp>
2022-10-10 22:22:09 +0900

    修正4

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,18 @@
 class FavoritesController < ApplicationController
+  before_action :set_blog, only: [:create, :destroy]
+
   def create
-    current_user.favorites.create(blog_id: params[:blog_id])
+    current_user.favorites.create(blog_id: params[:blog_id]).save
     # redirect_to blogs_path
   end
 
   def destroy
     current_user.favorites.find_by(id: params[:id]).destroy
     # redirect_to blogs_path
+  end
+
+  private
+  def set_blog
+    @blog = Blog.find(params[:blog_id])
   end
 end
